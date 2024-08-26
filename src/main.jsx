6a41be644779e44e56e1c7f4d040ea74349c45fa
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-08-26 11:20:09 -0500

    task 0

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,17 @@
 import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  // Make sure to import your App component
+import './index.css';
+
+// Create a root element
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render the App component to the root element
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 // Render the App component to the root element
