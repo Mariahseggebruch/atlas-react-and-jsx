1da9acf8de9bd3f4c8ad061508ac99e08775bc40
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-08-26 12:19:35 -0500

    pushing header component before I attempt to update app.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// src/components/Header.jsx
+import React from 'react';
+import Greeting from './Greeting';
+import SocialLinks from './SocialLinks';
+
+const Header = () => {
+  return (
+    <nav>
+      <Greeting />
+      <SocialLinks />
+    </nav>
+  );
+};
+
+export default Header;
