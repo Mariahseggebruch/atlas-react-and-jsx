ddc5eef5c4452b0aec5aec69a4a20f824d93727c
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-08-29 19:44:06 -0500

    imported headshot that I had to find

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import profilePicture from '../assets/your-profile-picture.jpg'
 
 const AboutMe = () => {
   return (
     <div className="about-me">
-      <img src="/src/assets/your-profile-picture.jpg" alt="Your Profile Picture" />
+      <img src={profilePicture} alt='headshot' />
       <p>
         Hi, I'm Mariah Jo. I'm a full-stack student in my 5th trimester. I'm passionate about working with Python, Javascript, and React. After graduation, I hope to pursue a career in Project Management or Tech Sales. In my free time, I enjoy seeing live music across the country, camping or hiking, and playing with my soon to be 1 year old corgi, King Arthur.
       </p>
